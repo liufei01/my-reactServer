1bf44e798049fbc74038217017a08aec1558a118
feiliu <965815085@qq.com>
2020-12-25 22:36:47 +0800

    '测试socketIo，完善消息列表和读消息接口'

--- db/models.js
+++ db/models.js
@@ -40,8 +40,21 @@ const userSchema = mongoose.Schema({
 // 定义Model,与集合对应，可以操作集合
 const UserModel = mongoose.model('user', userSchema)
 
+//定义chats集合的文档结构
+const chatSchema=mongoose.Schema({
+  from:{type:String,required:true},//发送用户的id
+  to:{type:String,required:true},//接收用户的id
+  chat_id:{type:String,required:true},//from和to组成的字符串
+  content:{type:String,required:true},//内容
+  read:{type:Boolean,default:false},//标识是否已读
+  create_time:{type:Number}//创建时间
+})
+//定义能操作chats集合数据的Model
+const ChatModel=mongoose.model('chat',chatSchema)
+
 // 向外暴露Model
 
 // module.exports=xxxx //一次性暴露
 
 exports.UserModel=UserModel   //分别暴露
+exports.ChatModel=ChatModel   //分别暴露
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 const UserModel = require('../db/models').UserModel
+const ChatModel = require('../db/models').ChatModel
 
 const MD5 = require('blueimp-md5')
 const filter = { password: 0, __v: 0 } //查询时过滤出指定的属性
@@ -98,8 +99,8 @@ router.get('/user', function (req, res) {
 })
 
 // 获取用户列表（根据类型）
-router.get('/userlist', function (req, res) {
-  const { type } = req.query
+router.post('/userlist', function (req, res) {
+  const { type } = req.body
   UserModel.find({ type }, filter, function (err, list) {
     if (!list) {
       res.send({ code: 1, msg: '查询用户列表失败' })
@@ -108,4 +109,54 @@ router.get('/userlist', function (req, res) {
     res.send({ code: 0, data: list, msg: new Date() })
   })
 })
+
+// 获取用户消息列表
+router.get('/msglist', function (req, res) {
+  // 获取cookie中的userId
+  const userId = req.cookies.userId
+  // 查询所有的用户列表
+  UserModel.find(function (err, uselist) {
+    // 用对象存储所有的user信息，key为user的id，value为用户的名称和头像
+    const users = {}
+    userlist.forEach(user => {
+      users[user._id] = {
+        username: user.username,
+        header: user.header
+      }
+    })
+    // 查询所有和userId相关的聊天信息
+    ChatModel.find(
+      { $or: [{ from: userId }, { to: userId }] },
+      filter,
+      function (err, chatMsgs) {
+        if (chatMsgs) {
+          res.send({ code: 0, data: { users, chatMsgs } })
+        } else {
+          res.send({ code: 1, msg: '请求数据有误' })
+        }
+      }
+    )
+  })
+})
+
+// 修改指定消息为已读
+router.post('/readmsg',function (req,res) { 
+  // 得到请求中的from和to
+  const from=req.body.from
+  const to=req.cookies.userId
+  // 去更新数据库中的数据
+  /**
+   * 参数1、更新条件
+   * 参数2、更新为指定的数据对象
+   * 参数3、是否一次更新多条，默认只更新一条
+   * 参数4、更新完成的回调函数
+   */
+  ChatModel.update({from,to,read:false},{read:true},{multi:true},function (err,doc) { 
+    if(doc){
+      res.send({code:0,data:doc.nModified})  //更新的数据数量
+    }else{
+      res.send({code:1,msg:'修改数据失败'})
+    }
+   })
+ })
 module.exports = router
